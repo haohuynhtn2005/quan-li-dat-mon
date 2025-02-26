199614c42abcfa88ba940ddf70e7bb6ba9702d0d
Hao <192075106+haohuynhtn2005@users.noreply.github.com>
2025-02-27 01:37:49 +0700

    Add order

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -11,10 +11,10 @@
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
+    {{-- <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{ asset('css/nunito.css') }}">
     <!-- Custom styles for this template-->
     <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
     @yield('head')
@@ -61,10 +61,10 @@
                 </a>
                 <div id="database" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="{{ route('table.index') }}">Table</a>
+                        <a class="collapse-item" href="{{ route('tables.index') }}">Table</a>
                         <a class="collapse-item" href="{{ route('food-types.index') }}">Food type</a>
                         <a class="collapse-item" href="{{ route('food-items.index') }}">Food item</a>
-                        <a class="collapse-item" href="{{ route('order.index') }}">Order</a>
+                        <a class="collapse-item" href="{{ route('orders.index') }}">Order</a>
                         <a class="collapse-item" href="{{ route('department.index') }}">Department</a>
                         <a class="collapse-item" href="{{ route('employee.index') }}">Employee</a>
                         <a class="collapse-item" href="{{ route('customer.index') }}">Customer</a>
